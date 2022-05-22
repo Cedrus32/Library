e320980a6c1d4598c4bc36af0ca2e986a18724df
Shannon <shannon@Shannons-MacBook-Pro.local>
2022-05-22 05:58:20 -0500

    Flesh out Book prototype, make buttons work to add a book to library in memory

--- script.js
+++ script.js
@@ -1,38 +1,74 @@
-let myLibrary = ['book1',
-                 'book2',
-                 'book3',];
+// -------------------- LIBRARY FUNCTIONS -------------------- //
+let myLibrary = [];
 
-function Book() {
+function Book(bookTitle, bookAuthor, bookPages, bookStatus) {
     // constructor for book object
+    this.title = bookTitle;
+    this.author = bookAuthor;
+    this.pages = bookPages;
+    this.status = bookStatus;
 }
 
-function addBook(book) {
-    // add book object to library
-    myLibrary.push(book);
+function makeBook() {
+    // * get inputs...
+    let bookTitle = document.getElementById('book-title').value;
+    let bookAuthor = document.getElementById('book-author').value;
+    let bookPages = document.getElementById('book-pages').value;
+    let bookStatus = document.getElementById('book-status').value;
+    // console.log(bookTitle + ' ' + bookAuthor + ' ' + bookPages + ' ' + bookStatus);
+    
+    // * create Book instance...
+    let newBook = new Book(bookTitle, bookAuthor, bookPages, bookStatus);
+    // console.log(newBook);
+
+    return newBook;
 }
 
-function displayLibrary(library) {
-    let book;
-    for (book in library) {
-        console.log(library[book]);
-    }
+function addBook() {
+    // * captures input, creates new Book
+    let newBook = makeBook();
+    // * add to library
+    myLibrary.push(newBook);
 }
 
+// function displayLibrary(myLibrary) {
+//     let book;
+//     for (book in myLibrary) {
+//         console.log(myLibrary[book]);
+//         // generate table
+//     }
+// }
+
+
+// -------------------- MAKE SAMPLE BOOKS -------------------- //
+
+
+// -------------------- BUTTONS -------------------- //
+
+// * brings popup form
 let add = document.getElementById('add');
 let popup = document.querySelector('.popup');
 add.addEventListener('click', () => {
     console.log('add');
+    // * open popup
     popup.classList.add('show');
 });
 
+// * makes & adds new book to library
 let confirm = document.getElementById('conf');
 confirm.addEventListener('click', () => {
     console.log('conf');
+    // * add book to library
+    addBook();
+    console.log(myLibrary);
+    // * close popup
     popup.classList.remove('show');
 });
 
+// * forgets input
 let cancel = document.getElementById('canc');
 cancel.addEventListener('click', () => {
     console.log('cancel');
+    // * close popup
     popup.classList.remove('show');
 });
